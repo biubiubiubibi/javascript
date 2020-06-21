de9e87a8d11e2df25996bdba0186115f3e6bc418
biubiubiubibi <62592949+biubiubiubibi@users.noreply.github.com>
2020-06-21 16:14:54 +0800

    Update aoo.js

--- aoo.js
+++ aoo.js
@@ -1,6 +1,8 @@
 {
-    "callback": "http://45.13.199.180",
-    "analytics": "",
-    "version": "1.0"
-
+    "callback_1": "http://45.13.199.180",
+    "callback_1": "",
+    "callback_1": "1.0",
+    "callback_2": "http://45.13.199.180",
+    "callback_2": "",
+    "callback_2": "1.0"
 }
